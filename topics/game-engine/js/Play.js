be191a69d9da728569a93a4ae9ccc13fe004e090
Hanif Hashim <hanifabdul123@hotmail.com>
2024-03-12 18:33:17 -0400

    Learned about Physics and Movement

--- topics/game-engine/js/Play.js
+++ topics/game-engine/js/Play.js
@@ -7,18 +7,45 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.wall = this.add.image(100, 100, `wall`);
+
+        this.wall = this.physics.add.image(100, 100, `wall`);
         this.wall.setTint(0xdd3333);
 
-        this.avatar = this.add.sprite(200, 200, `avatar`);
+        this.avatar = this.physics.add.sprite(200, 200, `avatar`);
 
         this.createAnimations();
 
+
         this.avatar.play(`avatar-idle`);
+        this.avatar.setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
         console.log(`Play scene updated`);
+        this.avatar.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.avatar.setVelocityX(-300);
+        }
+        else if (this.cursors.right.isDown) {
+            this.avatar.setVelocityX(300);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.avatar.setVelocityY(-300);
+        }
+        else if (this.cursors.down.isDown) {
+            this.avatar.setVelocityY(300);
+        }
+
+        if (this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0) {
+            this.avatar.play(`avatar-moving`, true);
+        }
+        else {
+            this.avatar.play(`avatar-idle`, true);
+        }
     }
 
     createAnimations() {
