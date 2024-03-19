54a11b60341ef98c0b7ed9628b76c859dbc170ab
Hanif Hashim <hanifabdul123@hotmail.com>
2024-03-19 17:15:42 -0400

    Revealed items from boxes

--- jams/game-jam/js/Boot.js
+++ jams/game-jam/js/Boot.js
@@ -1,59 +1,67 @@
 class Boot extends Phaser.Scene {
-    constructor() {
-        super(`preloader`);
-    }
-    preload() {
-        this.load.spritesheet(`sokoban`, `assets/sokoban_tilesheet.png`, {
-            frameWidth: 64
-        });
-    }
-    create() {
-        this.anims.create({
-            key: 'down-idle',
-            frames: [{ key: 'sokoban', frame: 52 }]
-        });
-        this.anims.create({
-            key: 'down-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', { start: 52, end: 54 }),
-            frameRate: 10,
-            repeat: -1
+  constructor() {
+    super(`preloader`);
+  }
+  preload() {
+    this.load.spritesheet(`sokoban`, `assets/sokoban_tilesheet.png`, {
+      frameWidth: 64
+    });
+    this.load.image(`bear`, `assets/bear.png`);
+    this.load.image(`chick`, `assets/chick.png`);
+    this.load.image(`duck`, `assets/duck.png`);
+    this.load.image(`parrot`, `assets/parrot.png`);
+    this.load.image(`penguin`, `assets/penguin.png`);
 
-        });
-        this.anims.create({
-            key: 'up-idle',
-            frames: [{ key: 'sokoban', frame: 55 }]
-        });
-        this.anims.create({
-            key: 'up-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', { start: 55, end: 57 }),
-            frameRate: 10,
-            repeat: -1
 
-        });
-        this.anims.create({
-            key: 'left-idle',
-            frames: [{ key: 'sokoban', frame: 81 }]
-        });
-        this.anims.create({
-            key: 'left-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', { start: 81, end: 83 }),
-            frameRate: 10,
-            repeat: -1
 
-        });
-        this.anims.create({
-            key: 'right-idle',
-            frames: [{ key: 'sokoban', frame: 78 }]
-        });
-        this.anims.create({
-            key: 'right-walk',
-            frames: this.anims.generateFrameNumbers('sokoban', { start: 78, end: 80 }),
-            frameRate: 10,
-            repeat: -1
+  }
+  create() {
+    this.anims.create({
+      key: 'down-idle',
+      frames: [{ key: 'sokoban', frame: 52 }]
+    });
+    this.anims.create({
+      key: 'down-walk',
+      frames: this.anims.generateFrameNumbers('sokoban', { start: 52, end: 54 }),
+      frameRate: 10,
+      repeat: -1
 
-        });
+    });
+    this.anims.create({
+      key: 'up-idle',
+      frames: [{ key: 'sokoban', frame: 55 }]
+    });
+    this.anims.create({
+      key: 'up-walk',
+      frames: this.anims.generateFrameNumbers('sokoban', { start: 55, end: 57 }),
+      frameRate: 10,
+      repeat: -1
 
-        this.scene.start(`game`);
-    }
+    });
+    this.anims.create({
+      key: 'left-idle',
+      frames: [{ key: 'sokoban', frame: 81 }]
+    });
+    this.anims.create({
+      key: 'left-walk',
+      frames: this.anims.generateFrameNumbers('sokoban', { start: 81, end: 83 }),
+      frameRate: 10,
+      repeat: -1
+
+    });
+    this.anims.create({
+      key: 'right-idle',
+      frames: [{ key: 'sokoban', frame: 78 }]
+    });
+    this.anims.create({
+      key: 'right-walk',
+      frames: this.anims.generateFrameNumbers('sokoban', { start: 78, end: 80 }),
+      frameRate: 10,
+      repeat: -1
+
+    });
+
+    this.scene.start(`game`);
+  }
 
 }
--- jams/game-jam/js/Game.js
+++ jams/game-jam/js/Game.js
@@ -1,3 +1,9 @@
+const level = [
+  [1, 0, 3],
+  [2, 4, 1],
+  [3, 4, 2]
+]
+
 class Game extends Phaser.Scene {
 
   /** @type {Phaser.Types.Input,Keyboard.CursorKeys}*/
@@ -8,6 +14,12 @@ class Game extends Phaser.Scene {
 
   /** @type {Phaser.Physics.Arcade.StaticGroup} */
   boxGroup
+
+  /** @type {Phaser.Physics.Arcade.Sprite} */
+  activeBox
+
+  /** @type {Phaser.GameObjects.Group} */
+  itemsGroup
 
   constructor() {
     super(`game`);
@@ -30,7 +42,9 @@ class Game extends Phaser.Scene {
     // this.boxGroup.get(width * 0.25, 150, `sokoban`, 10);
     this.createBoxes();
 
-    this.physics.add.collider(this.player, this.boxGroup);
+    this.itemsGroup = this.add.group();
+
+    this.physics.add.collider(this.player, this.boxGroup, this.handlePlayerBoxCollide, undefined, this);
   }
 
   createBoxes() {
@@ -39,13 +53,14 @@ class Game extends Phaser.Scene {
     let xPer = 0.25;
     let y = 150;
 
-    for (let row = 0; row < 3; ++row) {
+    for (let row = 0; row < level.length; ++row) {
 
-      for (let col = 0; col < 3; ++col) {
+      for (let col = 0; col < level[row].length; ++col) {
         /**@type {Phaser.Physics.Arcade.Sprite} */
         const box = this.boxGroup.get(width * xPer, y, 'sokoban', 10)
         box.setSize(64, 32)
           .setOffset(0, 32)
+          .setData('itemType', level[row][col])
         xPer += 0.25;
       }
 
@@ -55,7 +70,77 @@ class Game extends Phaser.Scene {
     }
   }
 
-  update() {
+  /**
+   * 
+   * @param {Phaser.Physics.Arcade.Sprite} player 
+   * @param {Phaser.Physics.Arcade.Sprite} box 
+   */
+  handlePlayerBoxCollide(player, box) {
+
+    if (this.activeBox) {
+      return;
+    }
+
+    this.activeBox = box;
+
+    this.activeBox.setFrame(9)
+  }
+  /**
+   * 
+   * @param {Phaser.Physics.Arcade.Sprite} box 
+   */
+  openBox(box) {
+    if (!box) {
+      return;
+    }
+    const itemType = box.getData('itemType');
+
+    /** @type {Phaser.GameObjects.Sprite} */
+    let item;
+
+    switch (itemType) {
+      case 0:
+        item = this.itemsGroup.get(box.x, box.y);
+        item.setTexture('bear');
+        break;
+      case 1:
+        item = this.itemsGroup.get(box.x, box.y);
+        item.setTexture('chick');
+        break;
+      case 2:
+        item = this.itemsGroup.get(box.x, box.y);
+        item.setTexture('duck');
+        break;
+      case 3:
+        item = this.itemsGroup.get(box.x, box.y);
+        item.setTexture('parrot');
+        break;
+      case 4:
+        item = this.itemsGroup.get(box.x, box.y);
+        item.setTexture('penguin');
+        break;
+    }
+
+    if (!item) {
+      return;
+    }
+
+    item.scale = 0
+    item.alpha = 0
+
+    this.tweens.add({
+      targets: item,
+      y: '-=50',
+      alpha: 1,
+      scale: 0.5,
+      duration: 500
+    });
+
+    this.activeBox = undefined;
+
+  }
+
+  updatePlayer() {
     const speed = 200;
 
     if (this.cursors.left.isDown) {
@@ -81,6 +166,36 @@ class Game extends Phaser.Scene {
       const direction = parts[0];
       this.player.play(`${direction}-idle`);
     }
+
+    const spaceJustPressed = Phaser.Input.Keyboard.JustUp(this.cursors.space);
+    if (spaceJustPressed && this.activeBox) {
+      this.openBox(this.activeBox);
+    }
+
+  }
+
+  updateActiveBox() {
+    if (!this.activeBox) {
+      return;
+    }
+
+    const distance = Phaser.Math.Distance.Between(
+      this.player.x, this.player.y,
+      this.activeBox.x, this.activeBox.y
+    );
+
+    if (distance < 64) {
+      return;
+    }
+
+    this.activeBox.setFrame(10);
+    this.activeBox = undefined;
+  }
+
+  update() {
+
+    this.updatePlayer();
+    this.updateActiveBox();
 
     this.children.each(c => {
       /** @type {Phaser.Physics.Arcade.Sprite} */
